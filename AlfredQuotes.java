24a087b00dcf89626d6b1f2c2ce2de5a69980c42
Brian Denmark <testingarea479@gmail.com>
2022-12-06 21:01:16 -0800

    init commit

---
+++ AlfredQuotes.java
@@ -0,0 +1,30 @@
+import java.util.Date;
+class AlfredQuotes {
+    public String greetWorld() {
+        return "Hello, world!";
+    }
+
+    public String basicGreeting() {
+        return "Hello, lovely to see you. How are you?";
+    }
+
+    public String guestGreeting(String name) {
+        // Your code here
+        return "place holder for guest greeting return string";
+    }
+
+    public String dateAnnouncement(Date date) {
+        // Your code here
+        return "place holder for date annoucnement return string";
+    }
+
+    public String respondBeforeAlexis(String conversation) {
+        // Your code here
+        return "for snarky response return string";
+    }
+
+    // NINJA BONUS
+    // See the specs to overload the guestGreeting method
+    // SENSEI BONUS
+    // Write your own AlfredQuote method using any of the string methods you have learned!
+}
---
+++ AlfredTest.java
@@ -0,0 +1,8 @@
+public class TestAlfredBot {
+    public static void main(String[] args) {
+        // Yer code here
+        AlfredBot alfredApp = new AlfredBot();
+        String alfredMessage = alfredApp.greetWorld();
+        System.out.println(alfredMessage);
+    }
+}
